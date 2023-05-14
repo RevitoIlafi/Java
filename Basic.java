7f8023a827805b114c5e5efc94363ff0c9683d41
Revito Ilafi Mac <ilafirevito@gmail.com>
2023-05-14 14:18:30 +0700

    Basic

---
+++ Basic.java
@@ -0,0 +1,36 @@
+public class Basic{
+    public static void main(String args[]){
+        // variables
+        int a = 95;
+        int b;
+        b = 75;
+        char c = 'a';
+        String d = "Hello";
+        double e = 17.5;
+        boolean f = true;
+        // byte g = 150;
+        long h = 2000;
+        System.out.println(f);
+        // operator
+        System.out.println(a + b);
+        System.out.println(a-b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+        System.out.println(a == b);
+        System.out.println(a != b);
+        System.out.println(a < b);
+        System.out.println(a > b);
+        System.out.println(a <= b);
+        System.out.println(a >= b);
+        System.out.println(!f);
+        System.out.println(true && false);
+        System.out.println(a == b || b > e);
+        System.out.println(b+=2);
+        System.out.println(b);
+        System.out.println(++b);
+        System.out.println(b--);
+        System.out.println(--b);
+        
+    }
+}
